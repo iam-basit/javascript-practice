4924c6159fbbeeeac1601c4399913c7481343f3e
Bsit Malik <basithamed246@gmail.com>
2023-07-10 15:07:43 +0530

    Coding challenge #1 Done ✔

--- assignments/script.js
+++ assignments/script.js
@@ -1,5 +1,33 @@
+// ** Lecture values and variables:
+/*
 let country = "Afghanistan";
 let continent = "Asia";
 let population = "40";
-
 console.log(country, population, continent);
+*/
+
+// ** Lecture Data types:
+//Log the types of 'isIsland', 'population', 'country' and 'language'to the console.
+/*
+const isIsland = false;
+let language;
+let country = "Afghanistan";
+let continent = "Asia";
+let population = "40";
+console.log(`
+${typeof(isIsland)}
+${typeof(population)}
+${typeof(country)}
+${typeof(language)}`)
+*/
+
+//** #Coding challenge 1
+/*
+let massMark = 78, heightMark = 169/100;
+let massJohn = 92, heightJohn = 195/100;
+//BMI = mass /(height * height)
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(Math.floor(BMIMark));
+console.log(Math.floor(BMIJohn));
+*/
