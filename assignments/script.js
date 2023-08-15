5de4f0facc771e2470f30db5558b7d6bf3e0accf
Bsit Malik <basithamed246@gmail.com>
2023-08-15 15:54:03 +0530

    Coding challenge #4 Done✔

--- assignments/script.js
+++ assignments/script.js
@@ -35,6 +35,27 @@ console.log(Math.floor(BMIJohn));
 //** #Coding challenge 2 
 
 /* Write your code below. Good luck! 🙂 */
+// 
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+// if(BMIJohn > BMIMark)
+// console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+
+// else{
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+// }
+
+//** #Coding challenge 3
+/* Write your code below. Good luck! 🙂 */
 // const scoreDolphins = (98+108+89)/3;
 // const scoreKoalas = (88+91+110)/3;
 
@@ -47,3 +68,14 @@ console.log(Math.floor(BMIJohn));
 // else{
 //    console.log("Both win the trophy");
 // }
+
+//** #Coding challenge 4
+
+// const bill = 275;
+
+// /* Write your code below. Good luck! 🙂 */
+
+
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15: bill * 0.2;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
